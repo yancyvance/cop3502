08d6e1e054181195e03873c7105f688e0dcdbe60
Yancy Vance <yancyvance@gmail.com>
2025-06-11 11:10:15 -0400

    Update vla.c

--- array/vla.c
+++ array/vla.c
@@ -5,14 +5,17 @@
 
 int main(void) {
     // create a list
+    VLArray *list = vla_create_list();
     
     
+    // perform some operations here
     
+    // add your codes
     
     
     
     // destroy the list
-    
+    vla_destroy_list(list);
     
     return 0;
 }
