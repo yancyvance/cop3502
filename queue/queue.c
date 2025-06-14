bf1b63e650571e4f73783b2f68b5bda2fe0ad327
Yancy Vance <yancyvance@gmail.com>
2025-06-14 19:43:05 -0400

    Added Queue Test

---
+++ queue/queue.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "queue_sll.h"
+//#include "queue_vla.h"
+
+// Sample C Implementation of a Queue.
+
+int main(void) {
+    // create a queue
+    Queue *queue = queue_create();
+    
+    // perform some operations here    
+    for(int i = 1; i <= 15; i++)
+        queue_enqueue(queue, i);
+    
+    // print the contents
+    queue_print(queue);
+    
+    // destroy the queue
+    queue_destroy(queue);
+    
+    return 0;
+}
