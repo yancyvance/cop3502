8da40905ff7b3b5399c2d43cf48c9afa8da23808
Yancy Vance <yancyvance@gmail.com>
2025-06-19 17:42:54 -0400

    Completed Stack

--- stack/stack_sll.h
+++ stack/stack_sll.h
@@ -22,19 +22,98 @@ typedef struct Stack_s {
 
 // create and destroy stack
 Stack * stack_create();
-void stack_destroy(Stack *);
+void stack_destroy(Stack *stack);
 
 // various operations, important
-void stack_push(Stack *, int);
-int stack_pop(Stack *);
-int stack_peek(Stack *);
-int stack_is_empty(Stack *);
-void stack_print(Stack *);
+void stack_push(Stack *stack, int value);
+int stack_pop(Stack *stack);
+int stack_peek(Stack *stack);
+int stack_is_empty(Stack *stack);
+void stack_print(Stack *stack);
 
 
 
 // function definitions
+Stack * stack_create() {
+    // dynamically allocate a pointer to stack
+    Stack *stack = malloc( sizeof(Stack) );
+    
+    // check if malloc was successful
+    if(stack == NULL) return NULL;
+    
+    // call helper function to dynamically
+    // allocate the linked list
+    stack->list = sll_create_list();
+    
+    return stack;
+}
 
+
+void stack_destroy(Stack *stack) {
+    // destroy dynamic fields first then
+    // destroy the struct itself (stack)
+    
+    // destroy the linked list
+    sll_destroy_list( stack->list );
+    
+    // destroy the dynamic struct / stack
+    free(stack);
+}
+
+void stack_push(Stack *stack, int value) {
+    // we call the helper function that
+    // performs add to head to a linked list,
+    // passing the value we want to add
+    sll_add_head( stack->list, value );
+}
+
+int stack_pop(Stack *stack) {
+    // we call the helper function that
+    // performs remove head to a linked list
+    // afterward, we extract the node's data
+    // then, it is our responsibility to
+    // deallocate this node per our design
+    SLLNode *node = sll_remove_head( stack->list );
+    
+    if(node) {
+        // extract data
+        int value = node->data;
+    
+        // deallocate the node
+        sll_destroy_node(node);
+        
+        return value;
+    }
+    
+    // special marker to indicate null
+    return -1;
+}
+
+int stack_peek(Stack *stack) {
+    // if the stack is empty, we return
+    // a special marker to indicate null
+    //if( sll_is_empty(stack->list) )   // equivalent logic
+    if( stack_is_empty(stack) )
+        return -1;
+    
+    // return the data of the head
+    // of the linked list
+    return stack->list->head->data;
+}
+
+int stack_is_empty(Stack *stack) {
+    // we call the helper function that
+    // checks if a linked list is empty
+    // return the same value
+    return sll_is_empty( stack->list );
+}
+
+void stack_print(Stack *stack) {
+    // we call the helper function that
+    // prints the contents of a linked
+    // list; for debugging purposes
+    sll_print_list( stack->list );
+}
 
 
 #endif              // End STACK_H -- Do not add any code below
--- stack/stack_vla.h
+++ stack/stack_vla.h
@@ -22,19 +22,88 @@ typedef struct Stack_s {
 
 // create and destroy stack
 Stack * stack_create();
-void stack_destroy(Stack *);
+void stack_destroy(Stack *stack);
 
 // various operations, important
-void stack_push(Stack *, int);
-int stack_pop(Stack *);
-int stack_peek(Stack *);
-int stack_is_empty(Stack *);
-void stack_print(Stack *);
+void stack_push(Stack *stack, int value);
+int stack_pop(Stack *stack);
+int stack_peek(Stack *stack);
+int stack_is_empty(Stack *stack);
+void stack_print(Stack *stack);
 
 
 
 // function definitions
+Stack * stack_create() {
+    // dynamically allocate a pointer to stack
+    Stack *stack = malloc( sizeof(Stack) );
+    
+    // check if malloc was successful
+    if(stack == NULL) return NULL;
+    
+    // call helper function to dynamically
+    // allocate a variable length array
+    stack->list = vla_create_list();
+    
+    // some minor tweak that prevents
+    // our array from growing
+    stack->list->can_grow = 0;
+    
+    return stack;
+}
 
+void stack_destroy(Stack *stack) {
+    // destroy dynamic fields first then
+    // destroy the struct itself (stack)
+    
+    // destroy the array
+    vla_destroy_list( stack->list );
+    
+    // destroy the dynamic struct / stack
+    free(stack);
+}
+
+void stack_push(Stack *stack, int value) {
+    // we call the helper function that
+    // adds at the tail-end of an array,
+    // passing the value we want to add
+    vla_add_tail( stack->list, value );
+}
+
+int stack_pop(Stack *stack) {
+    // we call the helper function that
+    // removes from the tail-end of an array
+    return vla_remove_tail( stack->list );
+}
+
+int stack_peek(Stack *stack) {
+    // compute the index of the top element
+    // based on the current size of the array
+    int top_idx = stack->list->size - 1;
+    
+    // if the stack is empty, we return
+    // a special marker to indicate null
+    //if( top_idx == -1 )       // equivalent logic
+    if( stack_is_empty(stack) )
+        return -1;
+    
+    // return the element at this location
+    return stack->list->array[top_idx];
+}
+
+int stack_is_empty(Stack *stack) {
+    // we call the helper function that
+    // checks if an array is empty
+    // return the same value
+    return vla_is_empty( stack->list );
+}
+
+void stack_print(Stack *stack) {
+    // we call the helper function that
+    // prints the contents of an array;
+    // for debugging purposes
+    vla_print_list( stack->list );
+}
 
 
 #endif              // End STACK_H -- Do not add any code below
