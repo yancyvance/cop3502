22b5f64da3e3561464a770bc2cf9a6d53f90b2e5
Yancy Vance <yancyvance@gmail.com>
2025-07-20 23:01:15 -0400

    Completed trie

--- trie/trie.c
+++ trie/trie.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include "trie.h"
+#define WORD_COUNT 6
 
 // Sample Test Program for Trie.
 
 int main(void) {
     // create a trie
     Trie *trie = trie_create();
-
+    
+    // array of strings
+    char words[WORD_COUNT][255] = {
+        "hello", "hi", "helios", "hope", "hopeful", "hopefully"
+    };
 
     // insert the following strings
-    trie_insert(trie, "hello");
-    trie_insert(trie, "hi");
-    trie_insert(trie, "helios");
-    trie_insert(trie, "hope");
-    trie_insert(trie, "hopeful");
-    trie_insert(trie, "hopefully");
-
+    for(int i = 0; i < WORD_COUNT; i++)
+        trie_insert(trie, words[i]);
+    
     // do a lookup
     printf("%d\n", trie_search(trie, "hi"));
     printf("%d\n", trie_search(trie, "hello"));
     printf("%d\n", trie_search(trie, "hopeful"));
+    printf("%d\n", trie_search(trie, "world"));
+    printf("%d\n", trie_search(trie, "hop"));
 
     // remove string
     trie_remove(trie, "hopefully");
@@ -28,6 +31,11 @@ int main(void) {
     printf("%d\n", trie_search(trie, "hopeful"));
     printf("%d\n", trie_search(trie, "hopefully"));
 
+    // remove all the words
+    for(int i = 0; i < WORD_COUNT; i++)
+        trie_remove(trie, words[i]);
+    
+    printf("%d\n", trie_search(trie, "hopeful"));
 
     // deallocate trie
     trie_destroy(trie);
--- trie/trie.h
+++ trie/trie.h
@@ -5,7 +5,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#define LETTER_COUNT 26
+#include <string.h>
+#define MAX_CHILD_COUNT 26
 
 // Sample C Implementation of a Trie.
 
@@ -15,7 +16,7 @@
 // Node that only has pointers to all
 // the possible 26 child nodes.
 typedef struct TrieNode_s {
-    struct TrieNode_s *children[LETTER_COUNT];
+    struct TrieNode_s *children[MAX_CHILD_COUNT];
     int is_end_of_word;
 } TrieNode;
 
@@ -35,6 +36,7 @@ void trie_destroy_node(TrieNode *node);
 // create and destroy trie
 Trie * trie_create();
 void trie_destroy(Trie *trie);
+void trie_destroy_recursive(TrieNode *node);
 
 // various operations, important
 void trie_insert(Trie *trie, char *word);
@@ -44,15 +46,180 @@ int trie_remove_recursive(TrieNode *node, char *word, int len, int pos);
 
 // helper functions
 int trie_has_children(TrieNode *node);
+int trie_is_marked(TrieNode *node);
 int trie_char_to_index(char alpha);
 char trie_index_to_char(int idx);
 
 
 
-
-
 // function definitions
 
+// create and destroy nodes
+TrieNode * trie_create_node() {
+    TrieNode *node = malloc( sizeof(TrieNode) );
+    
+    if(node == NULL) return NULL;
+    
+    // initialize the fields
+    node->is_end_of_word = 0;
+    
+    // loop through all the child pointers
+    // and set them all to NULL
+    for(int i = 0; i < MAX_CHILD_COUNT; i++)
+        node->children[i] = NULL;
+        
+    return node;
+}
+
+void trie_destroy_node(TrieNode *node) {
+    // deallocate the node itself
+    free(node);
+}
+
+// create and destroy trie
+Trie * trie_create() {
+    Trie *trie = malloc( sizeof(Trie) );
+    
+    if(trie == NULL) return NULL;
+    
+    // create the root
+    trie->root = trie_create_node();
+    
+    return trie;
+}
+
+void trie_destroy(Trie *trie) {
+    // partially implemented
+    trie_destroy_recursive(trie->root);
+    
+    free(trie);
+}
+
+void trie_destroy_recursive(TrieNode *node) {
+    // base case
+    if(node == NULL)
+        return;
+        
+    // do a postorder traversal, LRV
+    for(int i = 0; i < MAX_CHILD_COUNT; i++) {
+        trie_destroy_recursive(node->children[i]);
+    }
+    
+    // deallocate this node
+    trie_destroy_node(node);
+}
+
+void trie_insert(Trie *trie, char *word) {
+    // get the length of the string
+    int len = strlen(word);
+    TrieNode *ptr = trie->root;
+    
+    for(int i = 0; i < len; i++) {
+        char alpha = word[i];
+        int idx = trie_char_to_index(alpha);
+        
+        // check if there is an existing path
+        if(!(ptr->children[idx])) {
+            // if there is none, create one
+            ptr->children[idx] = trie_create_node();
+        }
+
+        // then go to that path
+        ptr = ptr->children[idx];
+    }
+    
+    // finally, mark the last node
+    ptr->is_end_of_word = 1;
+}
+
+int trie_search(Trie *trie, char *word) {
+    // get the length of the string
+    int len = strlen(word);
+    TrieNode *ptr = trie->root;
+    
+    for(int i = 0; i < len; i++) {
+        char alpha = word[i];
+        int idx = trie_char_to_index(alpha);
+        
+        // check if there is an existing path
+        if(!(ptr->children[idx])) {
+            // if there is none, terminate
+            return 0;
+        }
+
+        // go to that path
+        ptr = ptr->children[idx];
+    }
+    
+    // finally, check if the last node
+    // is a marker
+    return ptr->is_end_of_word == 1;
+}
+
+void trie_remove(Trie *trie, char *word) {
+    int len = strlen(word);
+    
+    // wrapper to remove a word
+    trie_remove_recursive(trie->root, word, len, 0);
+}
+
+int trie_remove_recursive(TrieNode *node, char *word, int len, int pos) {
+    // base case
+    if(len == pos) {
+        // check if this is a marker
+        if(node->is_end_of_word) {
+            // unmark the node
+            node->is_end_of_word = 0;
+            return 1;
+        }
+        else
+            return 0;
+    }
+    
+    // get the character and the corresponding
+    // index for the child node of that letter
+    char alpha = word[pos];
+    int idx = trie_char_to_index(alpha);
+    
+    if(!(node->children[idx]))
+        return 0;
+    
+    // go to the next position
+    int has_removed = trie_remove_recursive(node->children[idx], word, len, pos+1);
+    
+    // if didn't remove, report it
+    if( !has_removed ) return 0;
+    
+    // check if this is a node we can delete
+    // we cannot delete if this node has other child
+    // or it is a marked node
+    if( trie_has_children(node->children[idx]) ||  trie_is_marked(node->children[idx]) )
+        return 0;
+    else {
+        // deallocate that node
+        trie_destroy_node( node->children[idx] );
+        
+        // set the field to NULL
+        node->children[idx] = NULL;
+        
+        return 1;
+    }
+}
+
+int trie_has_children(TrieNode *node) {
+    for(int i = 0; i < MAX_CHILD_COUNT; i++)
+        if(node->children[i])
+            return 1;
+    return 0;
+}
+
+int trie_is_marked(TrieNode *node) {
+    return node->is_end_of_word;
+}
+
+int trie_char_to_index(char alpha) {
+    return (int)(alpha-'a');
+}
 
 
 #endif               // End TRIE_H -- Do not add any code below
