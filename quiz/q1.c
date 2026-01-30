ee55658fac786e11bdceac844c4bf8086d3715f5
Yancy Vance <yancyvance@gmail.com>
2026-01-30 18:28:09 -0500

    Create q1.c

---
+++ quiz/q1.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <string.h>
+#include "q1.h"
+
+/*
+    COP 3502C Q1
+    This program is written by: Your Full Name
+
+    This file is intentionally incomplete to simulate a written exam.
+    Further, you will not be provided the contents of the q1.h file.
+
+    You should first work out your solution using pencil and paper.
+    Once you are confident in your answer, type the final solution
+    into this file.
+
+    You may ONLY write code inside the bodies of the required functions.
+    Do NOT modify function signatures, include directives, or any other
+    part of this file.
+
+    Write your answers using a plain text editor only. Do NOT use a
+    compiler or an IDE while completing this file.
+
+    You may submit multiple times on Gradescope before the submission 
+    deadline. Only the final submission before the deadline will be graded.
+*/
+
+
+Point **transform_points(int *arr1, int *arr2, int size) {
+    // TODO: Complete this function
+    // TODO 1 BEGIN
+
+
+
+
+
+    // TODO 1 END
+}
+
+
+Student *create_student(char *name, char courses[][101], int count, int year) {
+    // TODO: Complete this function
+    // TODO 2 BEGIN
+
+
+
+
+
+    // TODO 2 END
+}
+
+
+void destroy_student(Student *s) {
+    // TODO: Complete this function
+    // TODO 3 BEGIN
+
+
+
+
+
+    // TODO 3 END
+}
+
+
+Account *merge_accounts(const Account *a1, const Account *a2) {
+    // TODO: Complete this function
+    // TODO 4 BEGIN
+
+
+
+
+
+    // TODO 4 END
+}
