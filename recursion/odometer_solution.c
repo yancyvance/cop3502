a18587c84485f9bfae3fb33287adcd54c15db886
Yancy Vance <yancyvance@gmail.com>
2026-02-04 12:47:46 -0500

    Create odometer_solution.c

---
+++ recursion/odometer_solution.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Write a program that enumerates all numbers of length 4
+// that has at least three 8s in it.
+
+// Function Prototypes
+void solve(int *arr, int n, int p);
+void print_array(int *arr, int n);
+int check_solution(int *arr, int n);
+
+
+int main(void) {
+    // Dynamically provide the slot information
+    int slots = 4;
+    
+    // Create the solution array that we will fill in
+    int *array = malloc(sizeof(int) * slots);
+    
+    // Start solving the problem by using the array
+    // as storage of the solution we are generating
+    solve(array, slots, 0);
+    
+    // Destroy the solution array
+    free(array);
+    
+    return 0;
+}
+
+
+// Function Definitions
+void solve(int *arr, int n, int p) {
+    // Base Case is when we have completely
+    // filled the solution array
+    if( p == n ) {
+        // Print the candidate solution found
+        // only if it is a correct one
+        int res = check_solution(arr, n);
+        
+        if( res )
+            print_array(arr, n);
+
+        return;
+    }
+    
+    for(int i = 0; i < 10; i++) {
+        // Try filling in this slot with i
+        // Note that because of the loop, we have
+        // ten possible branches, the 0 branch,
+        // the 1 branch, ... , the 9 branch
+        arr[p] = i;
+        
+        // Recursive Case is asking the function
+        // to go to the right and continue
+        // filling in the remaining slots, one
+        // slot at a time (per function call)
+        solve(arr, n, p+1);
+    }
+}
+
+void print_array(int *arr, int size) {
+    // Helper function that prints the contents
+    // of an array of integers with given size
+    for(int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int check_solution(int *arr, int size) {
+    // Helper function that returns 1 if the candidate
+    // is a correct solution to the problem; otherwise
+    // it returns 0
+    
+    // Count how many 8s there are in the array
+    int count = 0;
+    for(int i = 0; i < size; i++) {
+        if( arr[i] == 8 )
+            count++;
+    }
+    
+    // Final decision, are there at least three 8s?
+    return count >= 3;
+}
