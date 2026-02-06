bafd6cdbf1e49e8aaf8340e696f567af8c5e0abf
Yancy Vance <yancyvance@gmail.com>
2026-02-05 23:29:56 -0500

    Create permutation.c

---
+++ recursion/permutation.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Write a program that enumerates all permutation of 4 distinct symbols.
+
+// Function Prototypes
+void solve(int *arr, int n, int p, int *actual, int *is_used);
+void print_array(int *arr, int n);
+
+
+int main(void) {
+    // Dynamically provide the slot information
+    int slots = 4;
+    
+    // Create the solution array that we will fill in
+    // The elements used to fill in will be coming
+    // from the actual array of symbols
+    int *array = malloc(sizeof(int) * slots);
+    
+    // Assume that there are 4 numbers in the list
+    int symbols[] = {3, 6, 7, 9};
+    
+    // Dynamically allocate an array of integer (flags)
+    // that are all initialized to 0 (off/unused)
+    int *is_used = calloc(slots, sizeof(int));
+    
+    // Start solving the problem by using the array
+    // as storage of the solution we are generating
+    solve(array, slots, 0, symbols, is_used);
+    
+    // Destroy the solution array
+    free(array);
+    
+    // Destroy the flag array
+    free(is_used);
+    
+    return 0;
+}
+
+
+// Function Definitions
+void solve(int *arr, int n, int p, int *actual, int *is_used) {
+    // Base Case is when we have completely
+    // filled the solution array
+    if( p == n ) {
+        // Print the candidate solution found
+        print_array(arr, n);
+        return;
+    }
+    
+    // There are n symbols available to use
+    // Strategy: 3 parallel arrays
+    for(int i = 0; i < n; i++) {
+        // Try filling this slot with an unused 
+        // symbol only
+        if( !is_used[i] ) {
+            // Copy the actual symbol to the
+            // slot of the solution array
+            arr[p] = actual[i];
+            
+            // Mark the symbol as used
+            is_used[i] = 1;
+        
+            // Recursive Case is asking the function
+            // to go to the right and continue
+            // filling in the remaining slots, one
+            // slot at a time (per function call)
+            solve(arr, n, p+1, actual, is_used);
+            
+            // Unmark the symbol and try the next branch
+            is_used[i] = 0;
+        }
+    }
+}
+
+void print_array(int *arr, int size) {
+    // Helper function that prints the contents
+    // of an array of integers with given size
+    for(int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
