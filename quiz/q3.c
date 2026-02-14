c098b50a1715dfbbb8100ab1de97c5e57c784a32
Yancy Vance <yancyvance@gmail.com>
2026-02-14 17:10:15 -0500

    Create q3.c

---
+++ quiz/q3.c
@@ -0,0 +1,87 @@
+#include "q3.h"
+
+/*
+    COP 3502C Q3
+    This program is written by: Your Full Name
+
+    This file is intentionally incomplete to simulate a written exam.
+    Further, you will not be provided the contents of the q3.h file.
+
+    You should first work out your solution using pencil and paper.
+    Once you are confident in your answer, type the final solution
+    into this file.
+
+    You may ONLY write code inside the bodies of the required functions.
+    Do NOT modify function signatures, include directives, or any other
+    part of this file.
+
+    Write your answers using a plain text editor only. Do NOT use a
+    compiler or an IDE while completing this file. Save your file as q3.c
+
+    You may submit multiple times on Gradescope before the submission 
+    deadline. Only the final submission before the deadline will be graded.
+*/
+
+
+#ifdef T1
+void *solve(int *solution, int size) {
+    // TODO: Complete this function
+    // TODO 1 BEGIN
+
+    // Put your answers inside the curly braces
+    // and separate each value with a comma. An
+    // example is provided for reference.
+    
+    int answer[] = {
+        1, 2, 3, 4, 5               // If your answer is: 1, 2, 3, 4, 5
+    };
+    
+    // DO NOT MODIFY THIS PORTION
+    for(int i = 0; i < size; i++)
+        solution[i] = answer[i];
+
+    // TODO 1 END
+}
+#endif
+
+
+#ifdef T2
+void append(SLLNode *head1, SLLNode *head2) {
+    // TODO: Complete this function
+    // TODO 2 BEGIN
+
+
+
+
+
+    // TODO 2 END
+}
+#endif
+
+
+#ifdef T3
+SLLNode *merge(SLLNode *ptr1, SLLNode *ptr2) {
+    // TODO: Complete this function
+    // TODO 3 BEGIN
+
+
+
+
+
+    // TODO 3 END
+}
+#endif
+
+
+#ifdef T4
+SLLNode *reverse(SLLNode *head) {
+    // TODO: Complete this function
+    // TODO 4 BEGIN
+
+
+
+
+
+    // TODO 4 END
+}
+#endif
