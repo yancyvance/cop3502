b7733aa1ecfeb07de17233106bdc537769858c26
Yancy Vance <yancyvance@gmail.com>
2026-02-17 08:27:16 -0500

    Create lab5.c

---
+++ lab/lab05/lab5.c
@@ -0,0 +1,44 @@
+#ifdef T1
+const char *part1a(void) {
+    // Separate symbols with a single whitespace only.
+    // Do not include trailing spaces or newline characters.
+    // An example is provided for reference.
+    
+    // Assuming your answer is: 1 2 +
+    return "1 2 +";
+}
+#endif
+
+
+#ifdef T2
+double part1b(void) {
+    // Replace the return value with your answer.
+    // An example is provided for reference.
+    
+    // Assuming your answer is 3
+    return 3;
+}
+#endif
+
+
+#ifdef T3
+const char *part2a(void) {
+    // Separate symbols with a single whitespace only.
+    // Do not include trailing spaces or newline characters.
+    // An example is provided for reference.
+    
+    // Assuming your answer is: 1 2 +
+    return "1 2 +";
+}
+#endif
+
+
+#ifdef T4
+double part2b(void) {
+    // Replace the return value with your answer.
+    // An example is provided for reference.
+    
+    // Assuming your answer is 3
+    return 3;
+}
+#endif
