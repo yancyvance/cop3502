13b0b75887946c6748443774265e5112ec435fbb
Yancy Vance <yancyvance@gmail.com>
2026-02-22 14:55:50 -0500

    Create q4.c

---
+++ quiz/q4.c
@@ -0,0 +1,68 @@
+#include "q4.h"
+
+/*
+    COP 3502C Q4
+    This program is written by: Your Full Name
+
+    This file is intentionally incomplete to simulate a written exam.
+    Further, you will not be provided the contents of the q4.h file.
+
+    You should first work out your solution using pencil and paper.
+    Once you are confident in your answer, type the final solution
+    into this file.
+
+    You may ONLY write code inside the bodies of the required functions.
+    Do NOT modify function signatures, include directives, or any other
+    part of this file.
+
+    Write your answers using a plain text editor only. Do NOT use a
+    compiler or an IDE while completing this file. Save your file as q4.c
+
+    You may submit multiple times on Gradescope before the submission 
+    deadline. Only the final submission before the deadline will be graded.
+*/
+
+
+#ifdef T1
+const char *(int *solution, int size) {
+    // TODO: Complete this function
+    // TODO 1 BEGIN
+
+    // Put your answers between the double quotes.
+    // Note: The autograder is sensitive to whitespaces.
+    // An example is provided for reference.
+    
+    // Assuming your answer is: 1 2 3
+    return "1 2 3 ";
+
+    // TODO 1 END
+}
+#endif
+
+
+#ifdef T2
+void battle_game(Queue *gameQueue) {
+    // TODO: Complete this function
+    // TODO 2 BEGIN
+
+
+
+
+
+    // TODO 2 END
+}
+#endif
+
+
+#ifdef T3
+int is_balanced(char *exp) {
+    // TODO: Complete this function
+    // TODO 3 BEGIN
+
+
+
+
+
+    // TODO 3 END
+}
+#endif
