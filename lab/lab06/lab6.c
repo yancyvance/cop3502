8c10e5e9a429020aef46b17d50a75c7cad63f2b9
Yancy Vance <yancyvance@gmail.com>
2026-02-24 10:00:14 -0500

    Create lab6.c

---
+++ lab/lab06/lab6.c
@@ -0,0 +1,20 @@
+#ifdef T1
+double partb1(void) {
+    // Replace the return value with your answer.
+    // An example is provided for reference.
+    
+    // Assuming your answer is 3
+    return 3;
+}
+#endif
+
+
+#ifdef T2
+double partb2(void) {
+    // Replace the return value with your answer.
+    // An example is provided for reference.
+    
+    // Assuming your answer is 3
+    return 3;
+}
+#endif
