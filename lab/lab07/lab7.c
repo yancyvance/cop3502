83f8810d3d214db4dc7c297f30ddf7b2a7e23867
Yancy Vance <yancyvance@gmail.com>
2026-03-10 11:09:43 -0400

    Create lab7.c

---
+++ lab/lab07/lab7.c
@@ -0,0 +1,37 @@
+#ifdef T1
+const char *selection(void) {
+    // Separate numbers with a single whitespace only.
+    // Do not include trailing spaces or newline characters.
+    // An example is provided for reference.
+
+    // Input Scenario 1: What is the state of the array
+    // after i=2 of selection sort?
+    return "1 2 3 4 5";
+}
+#endif
+
+
+#ifdef T2
+const char *insertion(void) {
+    // Separate numbers with a single whitespace only.
+    // Do not include trailing spaces or newline characters.
+    // An example is provided for reference.
+
+    // Input Scenario 1: What is the state of the array
+    // after i=2 of insertion sort?
+    return "1 2 3 4 5";
+}
+#endif
+
+
+#ifdef T3
+const char *bubble(void) {
+    // Separate numbers with a single whitespace only.
+    // Do not include trailing spaces or newline characters.
+    // An example is provided for reference.
+
+    // Input Scenario 1: What is the state of the array
+    // after i=2 of bubble sort?
+    return "1 2 3 4 5";
+}
+#endif
