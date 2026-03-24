64bda24bd293384dc2c0d84506fc869c99ebe270
Yancy Vance <yancyvance@gmail.com>
2026-03-24 10:33:59 -0400

    Create lab8.c

---
+++ lab/lab08/lab8.c
@@ -0,0 +1,33 @@
+#ifdef T1
+const char *merge(void) {
+    // Separate numbers with a single whitespace only.
+    // Do not include trailing spaces or newline characters.
+    // An example is provided for reference.
+
+    // Merge Sort: What is the state of the array
+    // after the sixth merge opereation?
+    return "1 2 3 4 5";
+}
+#endif
+
+
+#ifdef T2
+const char *partition(void) {
+    // Separate numbers with a single whitespace only.
+    // Do not include trailing spaces or newline characters.
+    // An example is provided for reference.
+
+    // Partition: What is the state of the array
+    // after completing the partition step? In other words,
+    // what does the array look like in the final row?
+    return "1 2 3 4 5";
+}
+#endif
+
+#ifdef T3
+int version(void) {
+    // Binary Exponentiation: Which version is more efficient?
+    // Simply type in a number.
+    return 0;
+}
+#endif
