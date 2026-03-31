bc586a8e0d01d0363e90da662250faa7f9724347
Yancy Vance <yancyvance@gmail.com>
2026-03-31 11:44:51 -0400

    Create lab9.c

---
+++ lab/lab09/lab9.c
@@ -0,0 +1,26 @@
+#ifdef T1
+int height(BSTNode *root) {
+    // TODO: Complete this function
+    // TODO 1 BEGIN
+    
+    
+    
+    
+    
+    // TODO 1 END
+}
+#endif
+
+
+#ifdef T2
+int count_nodes_at_level(BSTNode *root, int k) {
+    // TODO: Complete this function
+    // TODO 2 BEGIN
+    
+    
+    
+    
+    
+    // TODO 2 END
+}
+#endif
