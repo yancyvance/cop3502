1a4b3c2f79aa1235cb83ab1c98097a6141d12629
Yancy Vance <yancyvance@gmail.com>
2026-04-03 17:35:19 -0400

    Create q6.c

---
+++ quiz/q6.c
@@ -0,0 +1,100 @@
+#include "q6.h"
+
+/*
+    COP 3502C Q6
+    This program is written by: Your Full Name
+
+    This file is intentionally incomplete to simulate a written exam.
+    Further, you will not be provided the contents of the q6.h file.
+
+    You should first work out your solution using pencil and paper.
+    Once you are confident in your answer, type the final solution
+    into this file.
+
+    You may ONLY write code inside the bodies of the required functions.
+    Do NOT modify function signatures, include directives, or any other
+    part of this file.
+
+    Write your answers using a plain text editor only. Do NOT use a
+    compiler or an IDE while completing this file. Save your file as q6.c
+
+    You may submit multiple times on Gradescope before the submission 
+    deadline. Only the final submission before the deadline will be graded.
+*/
+
+
+#ifdef T1
+void isBalanced(BTNode *r) {
+    // TODO: Complete this function
+    // TODO 1 BEGIN
+
+
+
+
+
+    // TODO 1 END
+}
+#endif
+
+
+#ifdef T2
+void twoChildren(BTNode *r) {
+    // TODO: Complete this function
+    // TODO 2 BEGIN
+
+
+
+
+
+    // TODO 2 END
+}
+#endif
+
+
+#ifdef T3
+int treeHeightDelete(void) {
+    // TODO: Complete this function
+    // TODO 3 BEGIN
+
+    // What is the height of the binary search tree
+    // after deleting the node?
+    // An example is provided for reference.
+    
+    // Assuming your answer is: 0
+    return 0;
+
+    // TODO 3 END
+}
+#endif
+
+
+#ifdef T4
+int fooAnswer(void) {
+    // TODO: Complete this function
+    // TODO 4 BEGIN
+
+    // What is the return value?
+    // An example is provided for reference.
+    
+    // Assuming your answer is: 0
+    return 0;
+
+    // TODO 4 END
+}
+#endif
+
+
+#ifdef T5
+int treeHeight(void) {
+    // TODO: Complete this function
+    // TODO 5 BEGIN
+
+    // What is the height of the binary search tree?
+    // An example is provided for reference.
+    
+    // Assuming your answer is: 0
+    return 0;
+
+    // TODO 5 END
+}
+#endif
